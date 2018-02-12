ad0685f808c7884e2ac1012cd418a44fe0219bc4
croberts1313 <croberts1313@bellsouth.net>
2018-02-12 17:29:09 -0600

    Add files via upload

---
+++ Terraform/FW_VM.tf
@@ -0,0 +1,45 @@
+resource "azurerm_availability_set" "FWAVS" {
+    name                = "avs-hs-fw"
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    managed             = "true"
+    platform_update_domain_count    = "5"
+    platform_fault_domain_count     = "2"
+}
+
+resource "azurerm_virtual_machine" "FW" {
+    name                            = "${var.firewall_name}"
+    location                        = "${var.location}"
+    resource_group_name             = "${azurerm_resource_group.rg.name}"
+    network_interface_ids           = ["${azurerm_network_interface.MGMT.id}", "${azurerm_network_interface.UNTRUST.id}", "${azurerm_network_interface.TRUST.id}", "${azurerm_network_interface.DMZ.id}"]
+    primary_network_interface_id    = "${azurerm_network_interface.MGMT.id}"
+    availability_set_id             = "${azurerm_availability_set.FWAVS.id}"
+    vm_size                         = "Standard_D3_v2"
+
+    plan {
+        name = "bundle2"
+        publisher = "paloaltonetworks"
+        product = "vmseries1"
+    }
+    storage_image_reference {
+        publisher   = "paloaltonetworks"
+        offer       = "vmseries1"
+        sku         = "bundle2"
+        version     = "latest"
+    }
+
+    storage_os_disk {
+        name = "${var.firewall_name}_OS"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name   = "${var.firewall_name}"
+        admin_username  = "${var.vm_username}"
+        admin_password  = "${var.vm_password}"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = "false"
+    }
+}
---
+++ Terraform/VARIABLES.tf
@@ -0,0 +1,113 @@
+
+/* Configure Azure Provider and declare all the Variables that will be used in Terraform configurations */
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
+
+variable "subscription_id" {
+  description = "Enter Subscription ID for provisioning resources in Azure"
+}
+
+variable "client_id" {
+  description = "Enter Client ID"
+}
+
+variable "client_secret" {
+  description = "Enter Client secret"
+}
+
+variable "tenant_id" {
+  description = "Enter Tenant ID"
+}
+
+variable "location" {
+  description = "The default Azure region for the resource provisioning"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network."
+}
+
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet"
+}
+
+variable "mgmt_subnet" {
+  description = "The address prefix to use for the mgmt subnet."
+}
+
+variable "untrust_subnet" {
+  description = "The address prefix to use for the untrust subnet."
+}
+
+variable "trust_subnet" {
+  description = "The address prefix to use for the trust subnet."
+}
+
+variable "dmz_subnet" {
+  description = "The address prefix to use for the dmz subnet."
+}
+
+variable "appgw_subnet" {
+  description = "The address prefix to use for the appgw subnet."
+}
+
+variable "subnet_name_prefix" {
+  description = "Subnet prefix for subnet names."
+  default     = "sn-"
+}
+
+variable "nsg_name_prefix" {
+  description = "Prefix for nsg names."
+  default     = "nsg_"
+}
+
+variable "firewall_name" {
+  description = "The name of the firewall"
+  default     = "PCCA-HSFW00"
+}
+
+variable "fw_username" {
+  description = "The username for the firewall"
+}
+
+variable "fw_password" {
+  description = "The password for the firewall"
+}
+
+variable "ILB_name" {
+  description = "The internal load balancer name"
+  default     = "BD-ILB"
+}
+
+variable "AppGw_name" {
+  description = "The application gateway name"
+  default     = "BD-AppGw"
+}
+
+variable "ssl_data" {
+  description = "Base64 version of .pfx file"
+}
+
+variable "ssl_password" {
+  description = "SSL certificate password"
+}
+
+variable "auth_cert_data" {
+  description = "Base64 version of .cer file"
+}
+
+variable "vm_username" {
+  description = "Enter local admin username for VMs"
+}
+
+variable "vm_password" {
+  description = "Ender local admin password for VMs"
+}
